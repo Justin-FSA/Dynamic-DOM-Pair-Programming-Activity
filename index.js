c270769edf4c17a8e3d2d915b4d144453e3c83a9
justin <jsattaur1016@yahoo.com>
2023-04-06 15:39:22 -0400

    commit

--- index.js
+++ index.js
@@ -3,37 +3,50 @@ class Bookmark{
         this.name = name;
         this.link = link;
     }
+
     render(){
         const bookmarkWrapper = document.createElement("li")
+
         const bookmarkLink = document.createElement("a");
         bookmarkLink.textContent = this.name
-        bookmarkLink.href = this.link
-        bookmarkWrapper.append(bookmarkLink)
+        bookmarkLink.href = this.link;
+
+        bookmarkWrapper.append(bookmarkLink);
+
         return bookmarkWrapper;
     }
 }
+
 class BookmarkMaintainer{
     constructor(){
         this.bookmarks = [];
     }
+
     add(bookmark){
         this.bookmarks.push(bookmark)
+        this.render()
     }
+
     render(){
-        const bookmarkList = document.getElementById("bookmarkList");
+        const bookmarkList = document.getElementById("bookmarksList");
+
         for(const bookmark of this.bookmarks){
             const renderedBookmark = bookmark.render()
             bookmarkList.append(renderedBookmark)
         }
     }
 }
-const BookmarkMaintainerInstance = new BookmarkMaintainer();
 
-document.getElementById("addBookmarkButton").addEventListener("click", () =>{
-const bookmarkLinkUserInput = document.getElementById("bookmarkLink").value;
-const bookmarkNameUserInput = document.getElementById("bookmarkName").value;
+const bookmarkMaintainerInstance = new BookmarkMaintainer();
 
-const bookmarkInstance = new Bookmark(bookmarkNameUserInput, bookmarkLinkUserInput);
+document.getElementById("addBookmarkButton").addEventListener("click", ()=>{
+    const bookmarkLinkUserInput = document.getElementById("bookmarkLink").value;
+    const bookmarkNameUserInput = document.getElementById("bookmarkName").value;
 
-BookmarkMaintainerInstance.add(bookmarkInstance)
+    const bookmarkInstance = new Bookmark(bookmarkNameUserInput, bookmarkLinkUserInput);
+
+    bookmarkMaintainerInstance.add(bookmarkInstance)
+    
 })
+
+document.getElementById("removeBookmarkButton").addEventListener("click")
